e30fe9276a1a8501a27b3464ea1396dfacc5dfba
Kuladeep-0112 <mittapallikuladeep@gmail.com>
2025-06-04 19:06:28 +0530

    feat: add TreeSetDemo with basic integer example

---
+++ src/main/java/com/collections/TreeSetDemo.java
@@ -0,0 +1,15 @@
+package com.collections;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        // Your code here
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        treeSet.add(10);
+        treeSet.add(20);
+        treeSet.add(30);
+        System.out.println(treeSet);
+    }
+}
+
